d9a58bef82718565145aa404ee37d82a2080d04f
rainlizard <15337628+rainlizard@users.noreply.github.com>
2024-05-26 23:13:41 +1000

    ver

--- autoload/Constants.gd
+++ autoload/Constants.gd
@@ -1,6 +1,6 @@
 @tool # Important for IncludeOnExport
 extends Node
-const VERSION = "0.07"
+const VERSION = "0.08"
 
 func _ready():
 	DisplayServer.window_set_title("CreatureMaker v" + str(VERSION))
